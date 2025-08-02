7216474c4b07267869c1646326e8b15aac500515
ZecoLP <zecolionelpratama@gmail.com>
2025-08-02 19:50:23 +0700

    Change BasicNft to BasicNFT to be able to deploy to sepolia testnet

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
-import {BasicNft} from "src/BasicNFT.sol";
+import {BasicNFT} from "src/BasicNFT.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
@@ -11,13 +11,16 @@ contract MintBasicNft is Script {
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
         mintNftOnContract(mostRecentlyDeployed);
     }
 
     function mintNftOnContract(address contractAddress) public {
         vm.startBroadcast();
-        BasicNft(contractAddress).mintNft(PUG);
+        BasicNFT(contractAddress).mintNft(PUG);
         vm.stopBroadcast();
     }
 }
